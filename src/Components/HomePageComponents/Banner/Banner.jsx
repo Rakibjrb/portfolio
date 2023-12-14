3b34d5f0bd8beb4a1c37875071cae42bc9ef30bc
rakibjrb <rakibjrb572157@gmail.com>
2023-12-14 14:58:51 +0600

    updated banner

--- src/Components/HomePageComponents/Banner/Banner.jsx
+++ src/Components/HomePageComponents/Banner/Banner.jsx
@@ -1,18 +1,35 @@
+import { useEffect } from "react";
 import { FaLinkedinIn, FaFacebookF } from "react-icons/fa";
 import { MdOutlineEmail } from "react-icons/md";
+import Typewriter from "typewriter-effect/dist/core";
 import bgshape from "../../../assets/images/bgshape.png";
 import myimage from "../../../assets/images/myimage.png";
 import "./banner.css";
 
 const Banner = () => {
+  useEffect(() => {
+    const bannerTitle = document.getElementById("writing-effect");
+    new Typewriter(bannerTitle, {
+      strings: ["Web Developer", "Front End Developer", "MERN Stack Developer"],
+      autoStart: true,
+      loop: true,
+      delay: 75,
+    });
+  }, []);
+
   return (
     <div className="overflow-x-hidden w-full h-[768px] md:h-[600px] xl:h-[720px] font-lato bg-gradient-to-b from-[#000] to-[#23c0d2]">
-      <div className="max-w-7xl mx-auto px-5 py-10 md:py-0 xl:px-0 h-full flex flex-col-reverse md:flex-row gap-5 items-center justify-center">
+      <div className="max-w-7xl mx-auto px-5 py-10 md:py-0 xl:px-0 h-full flex flex-col-reverse md:flex-row gap-5 items-center justify-center md:justify-between">
         <div className="lg:w-[70%] text-white space-y-4 text-center md:text-left mt-5 md:mt-0">
           <h3 className="text-3xl lg:text-4xl">Hello, {"I'ts"} Me</h3>
-          <h1 className="text-5xl lg:text-6xl font-extrabold">Rakibul Hasan</h1>
-          <h2 className="text-3xl lg:text-4xl">
-            I am a <span className="text-[#23c0d2]">Front End Developer</span>
+          <h1 className="text-5xl lg:text-6xl xl:text-7xl font-extrabold">
+            Rakibul Hasan
+          </h1>
+          <h2 className="text-3xl lg:text-4xl xl:text-5xl">
+            I am a{" "}
+            <span className="text-[#23c0d2]" id="writing-effect">
+              Front End Developer
+            </span>
           </h2>
           <div className="flex justify-center md:justify-start gap-3">
             <a className="cursor-pointer">
