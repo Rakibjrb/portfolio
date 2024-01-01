d4cf8d5fbc850facb8c1b97939c01d0f40606b7a
rakibjrb <rakibjrb572157@gmail.com>
2024-01-01 19:25:29 +0600

    completed about me section with responsive

--- src/Components/HomePageComponents/About/About.jsx
+++ src/Components/HomePageComponents/About/About.jsx
@@ -2,21 +2,25 @@ import image from "../../../assets/images/mypic2.jpg";
 
 const About = () => {
   return (
-    <div className="mt-24 max-w-7xl mx-auto px-3 xl:px-0 font-lato">
-      <div className="mt-5 flex gap-10">
-        <div className="w-1/3">
-          <img className="rounded-xl" src={image} alt="about section pic" />
+    <div className="my-24 max-w-7xl mx-auto px-3 md:px-10 lg:px-3 xl:px-0 font-lato">
+      <div className="mt-5  flex flex-col lg:flex-row lg:items-center gap-10">
+        <div className="xl:w-1/3">
+          <img
+            className="rounded-xl w-3/4 md:w-3/5 lg:w-auto mx-auto"
+            src={image}
+            alt="about section pic"
+          />
         </div>
-        <div className="text-white w-2/3">
+        <div className="text-white xl:w-2/3">
           <h2 className="text-5xl font-semibold mb-10">About Me</h2>
-          <p className="text-xl">
+          <p className="text-xl text-justify xl:text-left">
             Hello, I am Rakibul Hasan, a passionate Front End and MERN stack web
             developer with a keen eye for design and a love for crafting
             seamless user experiences. My journey in the world of web
             development has been an exciting adventure, marked by continuous
             learning and a commitment to delivering high-quality solutions.
           </p>
-          <p className="text-xl mt-5">
+          <p className="text-xl text-justify xl:text-left mt-5">
             I am fueled by the intersection of creativity and technology,
             constantly seeking innovative ways to bring ideas to life through
             code. With a strong foundation in HTML, CSS, and JavaScript, I
