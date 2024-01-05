a57bc64aaa24b75e180ee7509793737fb9e1e6bc
rakibjrb <rakibjrb572157@gmail.com>
2024-01-05 21:27:28 +0600

    completed latest projects card design

--- src/Components/HomePageComponents/LatestProjects/LatestProjects.jsx
+++ src/Components/HomePageComponents/LatestProjects/LatestProjects.jsx
@@ -1,9 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import SectionHeader from "../../CommonComponets/SectionHeader/SectionHeader";
+import Project from "../../CommonComponets/Project";
 
 const LatestProjects = () => {
+  const { data: projects } = useQuery({
+    queryKey: ["projects"],
+    queryFn: async () => {
+      const res = await axios.get("./projects.json");
+      return res.data;
+    },
+  });
+
   return (
     <div className="max-w-7xl mx-auto px-3 xl:px-0">
       <SectionHeader title={"Latest Projects"} />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-4 pt-5 md:pt-0">
+        {projects?.map((project) => (
+          <Project key={project.id} project={project} />
+        ))}
+      </div>
     </div>
   );
 };
