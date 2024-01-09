fc65d5061edb276640beada676da473417d41635
rakibjrb <rakibjrb572157@gmail.com>
2024-01-09 14:06:18 +0600

    updated banner mousemove animation and finished it

--- src/Components/HomePageComponents/Banner/Animation.jsx
+++ src/Components/HomePageComponents/Banner/Animation.jsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 
 const Animation = () => {
-  const arr = [...Array(10).keys()];
+  const makeCircles = 100;
+  const arr = [...Array(makeCircles).keys()];
 
   useEffect(() => {
     const cords = { x: 0, y: 0 };
@@ -13,28 +14,38 @@ const Animation = () => {
     });
 
     const animateCircles = () => {
-      circles?.forEach((circle) => {
-        circle.style.left = `${cords.x - 4}px`;
-        circle.style.top = `${cords.y}px`;
+      let x = cords.x;
+      let y = cords.y;
+      circles?.forEach((circle, index) => {
+        circle.style.left = `${x - 4}px`;
+        circle.style.top = `${y}px`;
+        circle.style.scale = (makeCircles - index) / makeCircles;
+        circle.x = x;
+        circle.y = y;
+        const allCicles = circles[index + 1] || circles[0];
+        x += (allCicles.x - x) * 0.3;
+        y += (allCicles.y - y) * 0.3;
       });
+      requestAnimationFrame(animateCircles);
     };
 
     window.addEventListener("mousemove", (e) => {
       cords.x = e.clientX;
       cords.y = e.clientY;
-      animateCircles();
     });
+
+    animateCircles();
   }, []);
 
   return (
-    <>
+    <div className="overflow-x-hidden">
       {arr?.map((element) => (
         <div
           key={`animation-div${element}`}
-          className="circle w-5 h-5 rounded-full bg-[#4cf3e0] absolute top-0 left-0 z-[1]"
+          className="circle w-5 h-5 rounded-full bg-[#4cf3e0] absolute top-0 left-0 z-[1] hidden lg:block"
         ></div>
       ))}
-    </>
+    </div>
   );
 };
 
