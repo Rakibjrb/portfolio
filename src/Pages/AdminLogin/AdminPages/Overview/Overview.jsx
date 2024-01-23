cb8f58fecfc3027e47d2d572cc36256916e9de39
rakibjrb <rakibjrb572157@gmail.com>
2024-01-23 21:54:29 +0600

    added icons on statistics bar

--- src/Pages/AdminLogin/AdminPages/Overview/Overview.jsx
+++ src/Pages/AdminLogin/AdminPages/Overview/Overview.jsx
@@ -1,14 +1,17 @@
 import moment from "moment";
 import Topbar from "./Topbar";
+import "./overview.css";
 
 const Overview = () => {
   return (
-    <div className="p-4">
-      <div className="text-white flex justify-between">
-        <h1 className="text-3xl">Admin Panel</h1>
-        <h2 className="text-3xl">{moment().format("DD/MM/YYYY")}</h2>
+    <div className="overview min-h-screen">
+      <div className="p-4">
+        <div className="text-white flex justify-between">
+          <h1 className="text-3xl">Admin Panel</h1>
+          <h2 className="text-3xl">{moment().format("DD/MM/YYYY")}</h2>
+        </div>
+        <Topbar />
       </div>
-      <Topbar />
     </div>
   );
 };
--- src/Pages/AdminLogin/AdminPages/Overview/Topbar.jsx
+++ src/Pages/AdminLogin/AdminPages/Overview/Topbar.jsx
@@ -1,19 +1,38 @@
+import { FaHeart } from "react-icons/fa";
+import { GoProjectSymlink } from "react-icons/go";
+import { AiOutlineFileDone } from "react-icons/ai";
+
 const Topbar = () => {
   return (
     <div className="mt-7 bg-[#4f4f4f] p-4">
       <h2 className="text-3xl mb-4 text-white">Statistics</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        <div className="p-3 bg-[#000000] rounded-md">
-          <h3 className="text-xl">Title</h3>
-          <h4 className="text-2xl font-bold">Value</h4>
+        <div className="p-3 bg-[#000000] rounded-md flex justify-center items-center min-h-[260px] text-white">
+          <div className="text-center">
+            <div className="flex justify-center">
+              <GoProjectSymlink className="text-5xl" />
+            </div>
+            <h3 className="text-xl my-5">Total Projects</h3>
+            <h4 className="text-2xl font-bold">6</h4>
+          </div>
         </div>
-        <div className="p-3 bg-[#000000] rounded-md">
-          <h3 className="text-xl">Title</h3>
-          <h4 className="text-2xl font-bold">Value</h4>
+        <div className="p-3 bg-[#000000] rounded-md flex justify-center items-center min-h-[260px] text-white">
+          <div className="text-center">
+            <div className="flex justify-center">
+              <FaHeart className="text-5xl" />
+            </div>
+            <h3 className="text-xl my-5">Total Hearts</h3>
+            <h4 className="text-2xl font-bold">6</h4>
+          </div>
         </div>
-        <div className="p-3 bg-[#000000] rounded-md">
-          <h3 className="text-xl">Title</h3>
-          <h4 className="text-2xl font-bold">Value</h4>
+        <div className="p-3 bg-[#000000] rounded-md flex justify-center items-center min-h-[260px] text-white">
+          <div className="text-center">
+            <div className="flex justify-center">
+              <AiOutlineFileDone className="text-5xl" />
+            </div>
+            <h3 className="text-xl my-5">Task Completed</h3>
+            <h4 className="text-2xl font-bold">2</h4>
+          </div>
         </div>
       </div>
     </div>
