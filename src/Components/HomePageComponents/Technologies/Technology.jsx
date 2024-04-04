69b9866d6cf5c0c787a3106eed1079c9d83ec09a
Rakibjrb <rakibjrb572157@gmail.com>
2024-04-04 16:28:03 +0600

    removed css animation from technologies that I know section

--- src/Components/HomePageComponents/Technologies/Technology.jsx
+++ src/Components/HomePageComponents/Technologies/Technology.jsx
@@ -6,12 +6,12 @@ const Technology = ({ tech }) => {
     <div className="py-10">
       <div className="relative">
         <div
-          className={`${tech.animation} w-32 h-[100px] flex justify-center items-center bg-white rounded-lg rounded-tl-3xl mr-10 cursor-pointer`}
+          className={`w-32 h-[100px] flex justify-center items-center bg-white rounded-lg rounded-tl-3xl mr-10 cursor-pointer`}
         >
           <img className="w-[60px] mx-auto" src={tech.img} alt="tech" />
         </div>
         <div
-          className={`${tech.animation} hover-do w-32 h-[100px] absolute top-0 left-0 bg-[#000] rounded-lg rounded-tl-3xl cursor-pointer flex items-center justify-center`}
+          className={`hover-do w-32 h-[100px] absolute top-0 left-0 bg-[#000] rounded-lg rounded-tl-3xl cursor-pointer flex items-center justify-center`}
         >
           <h2 className="text-[#23C0D2] text-xl text-center">
             {tech.technology}
