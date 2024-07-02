1a4c8463558fb5cf9546f8c57ec154b101266661
rakibjrb <rakibjrb572157@gmail.com>
2024-07-02 09:59:43 +0600

    added semster section on education page with static data

--- src/Pages/Education/MyEducation.jsx
+++ src/Pages/Education/MyEducation.jsx
@@ -1,9 +1,97 @@
 import Education from "../../Components/HomePageComponents/Education/Education";
 
+const semesters = [
+  {
+    passed: "Completed",
+    gpa: "3.30",
+  },
+  {
+    passed: "Completed",
+    gpa: "3.35",
+  },
+  {
+    passed: "Completed",
+    gpa: "Pending",
+  },
+  {
+    passed: "Running",
+    gpa: "Running",
+  },
+  {
+    passed: "Incomming",
+    gpa: "Incomming",
+  },
+  {
+    passed: "Incomming",
+    gpa: "Incomming",
+  },
+  {
+    passed: "Incomming",
+    gpa: "Incomming",
+  },
+  {
+    passed: "Incomming",
+    gpa: "Incomming",
+  },
+];
+
+const cardPosition = [
+  "start",
+  "center",
+  "end",
+  "center",
+  "start",
+  "center",
+  "end",
+  "center",
+];
+
 const MyEducation = () => {
   return (
     <div className="my-8 lg:my-12">
       <Education />
+      <div className="max-w-screen-lg mx-auto mt-16 px-4 xl:px-0">
+        <h2 className="text-4xl md:text-5xl text-center text-white">
+          Semesters
+        </h2>
+
+        <div className="mt-10 space-y-10 flex flex-col">
+          {semesters?.map((sem, index) => (
+            <div
+              key={sem + index + "semestercard"}
+              className={`flex ${`justify-${cardPosition[index]}`}`}
+            >
+              <div
+                data-aos="zoom-in-up"
+                data-aos-duration="1000"
+                className={`text-center space-y-3 py-14 px-6 w-80 text-white rounded-md ${
+                  (sem?.passed === "Completed" &&
+                    "bg-green-700 hover:bg-green-800") ||
+                  (sem?.passed === "Running" &&
+                    "bg-blue-700 hover:bg-blue-800") ||
+                  (sem?.passed === "Incomming" && "bg-red-700 hover:bg-red-800")
+                } transition-all duration-300 cursor-pointer`}
+              >
+                <h3 className="text-3xl">
+                  {index + 1}
+                  <sup>
+                    {(index === 0 && "st") ||
+                      (index === 1 && "nd") ||
+                      (index === 2 && "rd") ||
+                      (index > 2 && "th")}
+                  </sup>{" "}
+                  semester
+                </h3>
+                <p className="text-green-400 text-xl">{sem?.passed}</p>
+                <p>
+                  GPA : <span className="text-green-400">{sem?.gpa}</span> out
+                  of 4.00
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
